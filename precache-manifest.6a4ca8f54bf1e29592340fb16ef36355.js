3a2efe6b1636be242d0e183b117b526c69f28c28
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-06-16 23:28:26 -0400

    Updates

---
+++ precache-manifest.6a4ca8f54bf1e29592340fb16ef36355.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f636e9cb64143fe30745b1585ed3db",
+    "url": "/NetWorked/index.html"
+  },
+  {
+    "revision": "39a18a268670c4a8d019",
+    "url": "/NetWorked/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1224a78d7ddeea53e8ab",
+    "url": "/NetWorked/static/css/main.8e355de2.chunk.css"
+  },
+  {
+    "revision": "39a18a268670c4a8d019",
+    "url": "/NetWorked/static/js/2.063c3979.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NetWorked/static/js/2.063c3979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1224a78d7ddeea53e8ab",
+    "url": "/NetWorked/static/js/main.6f8a85de.chunk.js"
+  },
+  {
+    "revision": "d3d3e62b06161da97968",
+    "url": "/NetWorked/static/js/runtime-main.f94cd604.js"
+  }
+]);
