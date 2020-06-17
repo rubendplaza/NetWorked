5f801d683fd91612b5e0fa04f6d3ecd1b6981f46
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-06-16 23:22:24 -0400

    added error handling to cards component

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            hasError: false 
+        }
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true });
+    }
+
+    render(){
+        if(this.state.hasError) {
+            return (
+                <h1>Error Has Occurred</h1>
+            );
+        }
+        return (
+            this.props.children
+        );
+    }
+}
+
+export default ErrorBoundary;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import Header from '../components/Header';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends React.Component {
@@ -41,7 +42,9 @@ class App extends React.Component {
                     <Header />
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList connections={filteredConnections}/>
+                        <ErrorBoundary>
+                            <CardList connections={filteredConnections}/>
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );
