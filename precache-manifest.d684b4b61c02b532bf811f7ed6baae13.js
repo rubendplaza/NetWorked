5e2db925ad7e6e62707b7f9c3eb2ceabfdb9b935
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-07-19 18:42:58 -0400

    Updates

--- precache-manifest.d684b4b61c02b532bf811f7ed6baae13.js
+++ precache-manifest.d684b4b61c02b532bf811f7ed6baae13.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8db451a6fa13cabc4cb3416242a80179",
+    "revision": "c98088c87cd0f93812708eca6dac414f",
     "url": "/NetWorked/index.html"
   },
   {
-    "revision": "8198ba8059165493497b",
+    "revision": "7f56aa96cf587cf9a58a",
     "url": "/NetWorked/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "420417236357aaffa868",
+    "revision": "f461f4b8d39da3c620fc",
     "url": "/NetWorked/static/css/main.8e355de2.chunk.css"
   },
   {
-    "revision": "8198ba8059165493497b",
-    "url": "/NetWorked/static/js/2.b10bd322.chunk.js"
+    "revision": "7f56aa96cf587cf9a58a",
+    "url": "/NetWorked/static/js/2.8c6bcf9b.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/NetWorked/static/js/2.b10bd322.chunk.js.LICENSE.txt"
+    "url": "/NetWorked/static/js/2.8c6bcf9b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "420417236357aaffa868",
-    "url": "/NetWorked/static/js/main.ca27e8f7.chunk.js"
+    "revision": "f461f4b8d39da3c620fc",
+    "url": "/NetWorked/static/js/main.1cb147cd.chunk.js"
   },
   {
     "revision": "d3d3e62b06161da97968",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NetWorked/precache-manifest.ef1a7e8846191aa7ab59ad6138f38c16.js"
+  "/NetWorked/precache-manifest.d684b4b61c02b532bf811f7ed6baae13.js"
 );
 
 self.addEventListener('message', (event) => {
