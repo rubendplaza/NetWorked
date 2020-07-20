54f23afaf01c2ce9773115e0001685f3d2b9dc3f
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-07-19 20:30:41 -0400

    added labels to form elements to increase accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,13 +5,13 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className="ma4 search">
       <input
+        aria-label="Search Network Connections"
         id="search"
         className="bw2 pa3 ba"
         type="search"
         placeholder="Search Network"
         onChange={searchChange}
       />
-      <label for="search">Search Network</label>
     </div>
   );
 };
