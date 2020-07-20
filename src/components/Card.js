2d9fe90483e4025dc03eba15b36dca949a5fe86b
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-07-19 20:35:58 -0400

    changed the size of the api images

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 const Card = (props) => {
-    const { name, email, phone, company } = props;
-    return (
-        <div className='tc dib pa3 ma3 grow shadow-2 card ba bw2'>
-            <img alt='Profile Pic' src={`https://avatars.dicebear.com/api/avataaars/${email}.svg?w=250&h=200`}/>
-            <div>
-                <h2>{name}</h2>
-                <p>{company}</p>
-                <p>{phone}</p>
-                <p>{email}</p>
-            </div>
-        </div>
-    );
-}
+  const { name, email, phone, company } = props;
+  return (
+    <div className="tc dib pa3 ma3 grow shadow-2 card ba bw2">
+      <img
+        alt="Profile Pic"
+        src={`https://avatars.dicebear.com/api/avataaars/${email}.svg?w=200&h=150`}
+      />
+      <div>
+        <h2>{name}</h2>
+        <p>{company}</p>
+        <p>{phone}</p>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
